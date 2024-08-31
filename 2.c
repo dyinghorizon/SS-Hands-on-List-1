71fa14ff080f7086eca15051e436fdf8ae00c597
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-08-31 13:08:40 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,21 @@
+/*
+============================================================================
+Name : 2.c
+Author : Nishad Bagade
+Question : Write a simple program to execute in an infinite loop at the background. 
+		   Go to /proc directory and identify all the process related information 
+		   in the corresponding proc directory.
+Date: 10th - Aug - 2024
+
+Output: 
+nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand On List 1$ ./2 &
+[3] 59959
+(Look for the process in proc directory)
+============================================================================
+*/
+
+void main(){
+	while(1){
+		
+	}
+}
