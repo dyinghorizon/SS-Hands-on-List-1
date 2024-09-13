4a5de89030554d4fee0fcd35294d24aa2ad4b272
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-09-13 11:00:19 +0530

    Update 27b.c

--- 27b.c
+++ 27b.c
@@ -8,7 +8,6 @@ Date: 31nd - Aug - 2024
 
 Output: 
 nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand_On_List_1$ ./27b
-============OUTPUT USING execlp===============
 -rw-rw-r-- 2 nishad nishad 111 Aug 30 14:59 ./file_1
 ============================================================================
 */
@@ -21,8 +20,10 @@ void main()
     char *command_path = "/bin/ls";
     char *options = "-Rl";
     char *file_arg = "./file_1"; // Set if you want to execute `ls` command on a particular file
-
-    printf("============OUTPUT USING execlp===============\n");
+   
     execlp(command_path, command_path, options, file_arg, NULL);
     printf("\n");
 }
+
+//execlp is similar to execl, but it searches for the executable in the directories listed in the PATH environment variable, 
+//so you don't have to provide the full path to the executable.
