91db5f3572ebcad82c11eccc416c966c4954f318
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-09-13 10:30:05 +0530

    Update 27a.c

--- 27a.c
+++ 27a.c
@@ -9,7 +9,6 @@ Date: 31nd - Aug - 2024
 
 Output: 
 nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand_On_List_1$ ./27a
-============OUTPUT USING execl===============
 -rw-rw-r-- 2 nishad nishad 111 Aug 30 14:59 ./file_1
 ============================================================================
 */
@@ -20,10 +19,14 @@ nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand_On_List_1$ ./27a
 void main()
 {
     char *command_path = "/bin/ls";
-    char *options = "-Rl";
+    char *options = "-l";
     char *file_arg = "./file_1"; // Set if you want to execute `ls` command on a particular file
 
-    printf("============OUTPUT USING execl===============\n");
+    
     execl(command_path, command_path, options, file_arg, NULL);
     printf("\n");
 }
+
+//execl is the simplest form of the exec family. It takes the path to the executable as the first argument, 
+//followed by a list of arguments to pass to the new program (arg0, arg1, ..., NULL). 
+//The argument list must end with a NULL pointer.
