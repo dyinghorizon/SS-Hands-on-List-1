e2dcf20c2566871b3b425fa6f98e892648bbfa55
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-09-13 11:06:49 +0530

    Update 27d.c

--- 27d.c
+++ 27d.c
@@ -7,7 +7,6 @@ Date: 31nd - Aug - 2024
 
 Output: 
 nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand_On_List_1$ ./27d
-============OUTPUT USING execv===============
 --wx-wS--- 1 nishad nishad 111 Aug 31 18:59 ./file_2
 ============================================================================
 */
@@ -19,7 +18,9 @@ void main()
 {
     char *args[] = {"/bin/ls", "-Rl", "./file_2", NULL}; // Set the last but one element if you want to execute `ls` command on a particular file
 
-    printf("============OUTPUT USING execv===============\n");
     execv(args[0], args);
     printf("\n");
 }
+
+//execv is similar to execl, but instead of passing the arguments as separate parameters, you pass them as an array of strings (argv[]), where the last element must be NULL. 
+//This is useful when you want to pass the arguments programmatically (e.g., from a pre-constructed array).
