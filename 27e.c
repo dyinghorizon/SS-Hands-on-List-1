28ed300e22d5ccb9231ddc579d61a512129ee664
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-09-13 11:09:30 +0530

    Update 27e.c

--- 27e.c
+++ 27e.c
@@ -7,7 +7,6 @@ Date: 31nd - Aug - 2024
 
 Output: 
 nishad@nishad-ROG-Zephyrus-G14-GA401QM-GA401QM:~/Desktop/Hand_On_List_1$ ./27e
-============OUTPUT USING execvp===============
 --wx-wS--- 1 nishad nishad 111 Aug 31 18:59 ./file_2
 ============================================================================
 */
@@ -20,7 +19,9 @@ void main()
     // Command name, options, file name,
     char *args[] = {"/bin/ls", "-Rl", "./file_2", NULL}; // Set the last but one argument if you want to execute `ls` command on a particular file
 
-    printf("============OUTPUT USING execvp===============\n");
     execvp(args[0], args);
     printf("\n");
 }
+
+//execvp is like execv, but it searches for the program in the directories listed in the PATH environment variable, similar to execlp. 
+//You pass the arguments as an array (argv[]).
