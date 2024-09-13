f060463fc6115667ee23750fa2b99d3d2f9289b1
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-09-13 09:52:27 +0530

    Update 19.c

--- 19.c
+++ 19.c
@@ -16,21 +16,25 @@ Time taken : 25113
 #include <unistd.h>    
 #include <stdio.h>     
 
-
-// Import for __rdtsc()
+// The following part handles the import of the `__rdtsc()` function based on the compiler being used.
 #ifdef _MSC_VER
-#include <intrin.h>
+#include <intrin.h> // Include for __rdtsc() on Microsoft compilers (MSVC)
 #else
-#include <x86intrin.h>
+#include <x86intrin.h> // Include for __rdtsc() on GCC or other compilers
 #endif
+
+// The `__rdtsc()` function is a processor instruction that reads the Time Stamp Counter (TSC),
+// which counts the number of CPU cycles since the last reset, and provides a way to measure
+// very fine-grained timings.
+
 
 void main()
 {
     unsigned int start, end, pid;
 
-    start = __rdtsc();
+    start = __rdtsc(); //Capturing TSC value before getpid()
     pid = getpid();
-    end = __rdtsc();
+    end = __rdtsc(); //Capturing TSC value after getpid()
 
     printf("PID: %d\n", pid);
     printf("Time taken : %u \n", (end - start));
